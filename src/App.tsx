f5c2c188df5a3bc49269fa656474e3a1e0c70827
xanwerneck <alexandre.andreza@prof.infnet.edu.br>
2023-02-01 10:47:35 -0300

    remocao de referencias incorretas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import {useState, createContext} from 'react';
 import {default as Aula1} from './aulas/aula-1/Aula'
-import {default as Aula2} from './aulas/aula-2/Aula'
-import {default as Aula3} from './aulas/aula-3/Aula'
 
 export const AulaContext = createContext<{ slide: number, setSlide: Function }>({slide: 1, setSlide: () => {}})
 
